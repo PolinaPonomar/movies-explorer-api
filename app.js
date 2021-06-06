4d5063f2b7228511600aeb9210b8092f65bc135b
Polina Ponomareva <pp09117@gmail.com>
2021-06-07 01:52:51 +0300

    feat: реализовать логирование

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const { registerUser, login } = require('./controllers/users');
 const { usersRouter } = require('./routes/users');
 const { moviesRouter } = require('./routes/movies');
 const centralizedErrorHandling = require('./middlewares/centralizedErrorHandling');
+const NotFoundError = require('./errors/not-found-err');
 
 // Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -21,22 +23,20 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 // парсинг данных
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '60ba1b66110a37c51dfa5e58', // ХАРДКОД
-  };
-
-  next();
-});
+// подключаем логгер запросов
+app.use(requestLogger);
 
 app.post('/signup', registerUser);
 app.post('/signin', login);
 app.use(auth);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
+app.use(() => { throw new NotFoundError('Ресурс не найден'); }); // подумать над красотой
 
+// подключаем логгер ошибок
+app.use(errorLogger);
 // централизованная обработка ошибок
-app.use(centralizedErrorHandling);
+app.use(centralizedErrorHandling); // ErrorHander
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
